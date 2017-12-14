0dde01f9f602f95fe8c77a2f1768ac8d140bbdcf
Fredrik Larsson <flarsson93@gmail.com>
2017-12-14 18:07:21 +0100

    working on allowing vector of several pred in groups of 3

--- core.clj
+++ core.clj
@@ -35,3 +35,26 @@
      (filter 
       #(~(second where) (~(first where) %) ~@(nnext where)) 
       ~from))))
+
+
+(defmacro selecttest
+  "Acts like an SQL statement (e.g. 'SELECT [:name :id] from persons where [:id = 2] orderby :name').
+  Due to how it is constucted the where clause can use any clojure function that returns boolean and looks like [column op value] -> '(op column value)'"
+  [columns _ from _ where _ orderby]
+  `(map
+    #(select-keys % ~columns)
+    (sort-by 
+     ~orderby
+     (~(for [[colu op val] (partition 3 where)]
+        `(filter 
+          #(~op (~colu %) ~val) 
+          ~from)))))) ;; should end the filter list with ~from
+ 
+(defmacro testss
+  [where]
+  `(~(partition 3 where)))
+
+
+
+
+
