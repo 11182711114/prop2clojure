d887b362b158d91906edb343e7344c86d6cd5e2d
Fredrik Larsson <flarsson93@gmail.com>
2017-12-15 04:13:17 +0100

    finish unlimited predicate sql

--- core.clj
+++ core.clj
@@ -36,23 +36,20 @@
       #(~(second where) (~(first where) %) ~@(nnext where)) 
       ~from))))
 
-
-(defmacro selecttest
+(defmacro select
   "Acts like an SQL statement (e.g. 'SELECT [:name :id] from persons where [:id = 2] orderby :name').
-  Due to how it is constucted the where clause can use any clojure function that returns boolean and looks like [column op value] -> '(op column value)'"
+  Due to how it is constucted the where clause can use any clojure function that returns boolean and looks like [column op value] -> '(op column value)'
+  Works for any number of predicates in the where clause as long as they are in groups of 3, e.g. [:id > 0 :id < 3 :name not= 'isak']"
   [columns _ from _ where _ orderby]
   `(map
     #(select-keys % ~columns)
     (sort-by 
-     ~orderby
-     (~(for [[colu op val] (partition 3 where)]
-        `(filter 
-          #(~op (~colu %) ~val) 
-          ~from)))))) ;; should end the filter list with ~from
+      ~orderby
+      (filter 
+        #(every? true? (for [[colu# op# val#] (partition 3 ~where)] (op# (colu# %) val#))) 
+       ~from))))
  
-(defmacro testss
-  [where]
-  `(~(partition 3 where)))
+
 
 
 
